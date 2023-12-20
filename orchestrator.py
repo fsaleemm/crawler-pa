be56711c0d252366c5b531e4210d481e7aa5ed24
Farhat Saleem <fsaleemms@live.com>
2023-12-20 08:42:08 -0500

    crawler enhancements, change log, link expiration

--- orchestrator.py
+++ orchestrator.py
@@ -12,6 +12,9 @@ import queue, threading
 import uuid, base64, os, time, logging, requests
 from datetime import datetime
 
+from azure.cosmos import CosmosClient, PartitionKey
+import hashlib
+
 from dotenv import load_dotenv
 
 logging.basicConfig(filename=f'crawler-{time.time()}.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
@@ -22,32 +25,70 @@ load_dotenv()
 NUM_OF_THREADS = int(os.getenv("NUM_OF_THREADS", 1))
 EXCLUDE_LIST = os.getenv('EXCLUDE_LIST').split(',')
 BASE_URLS = os.getenv('BASE_URLS').split(',')
+EXTRACT_LINK_TYPE = [file_type.lower() for file_type in os.getenv('EXTRACT_LINK_TYPE').split(',')]
+CRAWL_URLS = os.getenv('CRAWL_URLS').split(',')
 
 enable_vectors_str = os.getenv("ENABLE_VECTORS", "false")
 ENABLE_VECTORS = enable_vectors_str.lower() in ['true', '1', 'yes']
 
-
-# Azure Search
 INDEX_NAME = os.getenv("INDEX_NAME", "crawler-index")
-SEARCH_ENDPOINT = os.getenv("SEARCH_ENDPOINT")
-SEARCH_CREDS = AzureKeyCredential(os.getenv("SEARCH_KEY"))
-
-index_client = SearchIndexClient(endpoint=SEARCH_ENDPOINT, credential=SEARCH_CREDS)
-search_client = SearchClient(
-    endpoint=SEARCH_ENDPOINT, credential=SEARCH_CREDS, index_name=INDEX_NAME
-)
 
 
-# Azure Form Recognizer
-FORM_RECOGNIZER_ENDPOINT = os.getenv("FORM_RECOGNIZER_ENDPOINT") 
-FORM_RECOGNIZER_CREDS = AzureKeyCredential(os.getenv("FORM_RECOGNIZER_KEY"))
+def setup_clients():
+    # Azure Search
+    
+    SEARCH_ENDPOINT = os.getenv("SEARCH_ENDPOINT")
+    SEARCH_CREDS = AzureKeyCredential(os.getenv("SEARCH_KEY"))
 
-form_recognizer_client = DocumentAnalysisClient(
-    endpoint=FORM_RECOGNIZER_ENDPOINT,
-    credential=FORM_RECOGNIZER_CREDS,
-)
+    index_client = SearchIndexClient(endpoint=SEARCH_ENDPOINT, credential=SEARCH_CREDS)
+    search_client = SearchClient(
+        endpoint=SEARCH_ENDPOINT, credential=SEARCH_CREDS, index_name=INDEX_NAME
+    )
+
+    # Azure Form Recognizer
+    FORM_RECOGNIZER_ENDPOINT = os.getenv("FORM_RECOGNIZER_ENDPOINT") 
+    FORM_RECOGNIZER_CREDS = AzureKeyCredential(os.getenv("FORM_RECOGNIZER_KEY"))
+
+    form_recognizer_client = DocumentAnalysisClient(
+        endpoint=FORM_RECOGNIZER_ENDPOINT,
+        credential=FORM_RECOGNIZER_CREDS,
+    )
+
+    # Azure CosmosDB Client
+    COSMOS_URL = os.environ.get("COSMOS_URL")
+    COSMOS_KEY = os.environ.get("COSMOS_DB_KEY")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "CrawlStore")
+    CONTAINER_NAME = os.environ.get("CONTAINER_NAME", "URLChangeLog")
+
+    cosmosdb_client = CosmosClient(COSMOS_URL, credential=COSMOS_KEY)
+    # Create the database if it does not exist
+    database = cosmosdb_client.create_database_if_not_exists(id=DATABASE_NAME)
+
+    # Define a partition key for the container
+    partition_key = PartitionKey(path="/url")
+
+    # Create the container if it does not exist
+    container = database.create_container_if_not_exists(id=CONTAINER_NAME, partition_key=partition_key)
+
+    return index_client, search_client, form_recognizer_client, cosmosdb_client, database, container
 
 
+def setup_crawler_data():
+    # Retrieve the stored MD5 hash
+    query = f"SELECT * FROM c"
+
+    crawler_store_items = list(container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
+
+    return crawler_store_items
+
+def url_in_crawler_store(url, items):
+    for item in items:
+        if item['url'] == url:
+            return True, item
+    return False, None
 
 def base_crawler_consumer(q, nextq):
     """Consumer function to process base URLs from the queue and add their links to the next queue."""
@@ -68,22 +109,30 @@ def crawl_base_url(base_url, nextq):
         with WebCrawler(base_url, exclude_urls=EXCLUDE_LIST) as crawler:
             logging.info(f"Crawling: {base_url}")
             crawler.visit_url(base_url)
+
+            md5_hash = hashlib.md5(crawler.get_page_source().encode()).hexdigest()
+
+            if not page_has_changed(url=base_url, md5_hash=md5_hash):
+                return
+
             table_dict = {}
-            table_dict = crawler.parse_tables()
+
+            # Change in crawl logic 12/4/2023 to reflect feedback
+            #table_dict = crawler.parse_tables()
 
             # if the page is not Opportunities page, then extract links and crawl the links. Assuming static content URL
-            if not table_dict:
-                logging.info(f"No links found on: {base_url}, assuming static content url.")
-                table_dict[base_url] = {}
-                body = crawler.get_elements(By.TAG_NAME, "body")
-                
-                if len(body) > 0:
-                    links = crawler.get_links(body[0], exclude=True)
+            #if not table_dict:
+                #logging.info(f"No links found on: {base_url}, assuming static content url.")
+            table_dict[base_url] = {}
+            body = crawler.get_elements(By.TAG_NAME, "body")
+            
+            if len(body) > 0:
+                links = crawler.get_links(body[0], exclude=True, file_types=EXTRACT_LINK_TYPE)
 
-                    table_dict[base_url]["links"] = links
-                    table_dict[base_url]["metadata"] = {}
-                else:
-                    logging.info(f"No body found on: {base_url}.")
+                table_dict[base_url]["links"] = links
+                table_dict[base_url]["metadata"] = {}
+            else:
+                logging.info(f"No body found on: {base_url}.")
 
             # Add links to the next queue
             for key_link, table in table_dict.items():
@@ -107,6 +156,7 @@ def url_crawler_consumer(q, nextq):
         result = crawl_url(item["url"])
         if result is not None:
             content, contenttype = result
+            
             item["content"] = content
             item["contenttype"] = contenttype
             nextq.put(item)
@@ -124,10 +174,21 @@ def crawl_url(url):
     try:
         if url.lower().endswith(".pdf"):
             response = requests.get(url)
+            md5_hash = hashlib.md5(response.content).hexdigest()
+
+            if not page_has_changed(url=url, md5_hash=md5_hash):
+                return None, None
+            
             return response.content, "pdf"
         else:
             with WebCrawler(base_url=url, exclude_urls=EXCLUDE_LIST) as crawler:
                 crawler.visit_url(url)
+
+                md5_hash = hashlib.md5(crawler.get_page_source().encode()).hexdigest()
+
+                if not page_has_changed(url=url, md5_hash=md5_hash):
+                    return None, None
+            
                 content = crawler.parse_page()
                 return content, "text"
     except Exception as e:
@@ -152,7 +213,7 @@ def chunker_consumer(q, nextq):
                 add_embeddings=ENABLE_VECTORS,
                 form_recognizer_client=form_recognizer_client if item["contenttype"] == "pdf" else None,
                 use_layout=True if item["contenttype"] == "pdf" else False,
-                metadata=item["metadata"]
+                metadata = item.get("metadata", None)
             )
 
             i=0
@@ -208,17 +269,84 @@ def indexer_consumer(q, search_client, batch_size=100):
 
     logging.info(f"Indexer Consumer is done")
 
+def page_has_changed(url, md5_hash):
+    
+    url_hash = hashlib.md5(url.encode()).hexdigest()
+    in_store, item = url_in_crawler_store(url, crawler_store_items)
+
+    if in_store:
+        stored_md5_hash = item['md5_hash']
+    else:
+        logging.info(f"Item does not exist in Crawler Store. {url}")
+        # If the item does not exist, add it to Cosmos DB
+        item = {'id': url_hash, 'url': url, 'md5_hash': md5_hash, 'not_seen_count' : 0}
+        container.upsert_item(body=item)
+        return True
+        
+
+    logging.info(f"For url: {url}  ---  md5_hash: {md5_hash}  ---  stored_md5_hash: {stored_md5_hash}")
+
+    # Compare the computed MD5 hash with the stored hash
+    if md5_hash != stored_md5_hash:
+        logging.info(f"Page content has changed. {url}")
+        # If the hash has changed, update the item in Cosmos DB
+        #item = items[0]
+        item['md5_hash'] = md5_hash
+        item['not_seen_count'] = 0
+        container.replace_item(item=item['id'], body=item)
+        return True
+    else:
+        logging.info(f"Page content has not changed. {url}")
+        return False
+
+def check_expired_links(crawler_store_items):
+    
+    logging.info(f"Checking expired links in crawler store.")
+
+    expired_links = []
+    for item in crawler_store_items:
+        url = item['url']
+        try:
+            response = requests.head(url)
+            if response.status_code == 404 or response.status_code == 403:
+
+                logging.info(f"Link unreachable, incrementing not seen count: {url}")
+
+                item['not_seen_count'] = item['not_seen_count'] + 1
+
+                if item['not_seen_count'] > 3:
+                    logging.info(f"Link unreachable for 3 consecutive crawls, expiring url: {url}")
+                    #container.delete_item(item=item['id'])
+                    expired_links.append(url)
+                else:
+                    container.replace_item(item=item['id'], body=item)
+
+        except Exception as e:
+            logging.error(f"Error occurred while checking {url}, Exception: {e}")
+    return expired_links
+
+def delete_from_index(expired_links, search_client):
+
+    logging.info(f"Deleteing expired links from index.")
+
+    # Query the index for the ids of the expired links
+    ids_to_delete = []
+    for link in expired_links:
+        results = search_client.search(search_text="*", select="id", filter=f"sourcefile eq '{link}'")
+        for result in results:
+            ids_to_delete.append(result["id"])
+
+    # Delete the expired links from the index
+    for id in ids_to_delete:
+        logging.info(f"Deleteing from index: id = {id}")
+        search_client.delete_documents(documents=[{"@search.action": "delete", "id": id}])
+
 def start_threads(consumer, source_queue, target_queue, num_of_threads):
     return [threading.Thread(target=consumer, args=(source_queue, target_queue)) for _ in range(num_of_threads)]
 
 
 def main():
   
-    start_time = time.time()
-    formatted_start_time = datetime.fromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")
-
-    logging.info(f"Starting crawler: {formatted_start_time}")
-
     create_search_index(index_name=INDEX_NAME, index_client=index_client)
 
     base_crawler_queue = queue.Queue()
@@ -244,6 +372,12 @@ def main():
     for base_url in BASE_URLS:
         base_crawler_queue.put(base_url)
 
+     # For each base url add it to base url queue
+    for url in CRAWL_URLS:
+        item = dict()
+        item["url"] = url
+        url_crawler_queue.put(item=item)
+
     # Wait for all the queues to be processed
     for q in queues:
         q.join()
@@ -257,6 +391,25 @@ def main():
     for thread in all_threads:
         thread.join()
 
+    # Check for removed links
+    expired_links = check_expired_links(crawler_store_items)
+    
+    # Remove links
+    delete_from_index(expired_links, search_client)
+
+    
+
+if __name__ == "__main__":
+    start_time = time.time()
+    formatted_start_time = datetime.fromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")
+
+    logging.info(f"Starting crawler: {formatted_start_time}")
+
+    index_client, search_client, form_recognizer_client, cosmosdb_client, database, container = setup_clients()
+
+    crawler_store_items = setup_crawler_data()
+
+    main()
 
     end_time = time.time()
     formatted_end_time = datetime.fromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")
@@ -264,6 +417,3 @@ def main():
 
     elapsed_time = end_time - start_time
     logging.info("Elapsed time: {:.2f} seconds".format(elapsed_time))
-
-if __name__ == "__main__":
-    main()
