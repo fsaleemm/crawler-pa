96ac26bcfa2ec30b55cd34d985e97053d1377bfa
Farhat Saleem <fsaleemms@live.com>
2026-02-18 10:17:11 -0500

    fix for borg pattern issue

--- crawler/Orchestrator.py
+++ crawler/Orchestrator.py
@@ -35,15 +35,15 @@ class Orchestrator:
             
             include_domains = os.getenv('INCLUDE_DOMAINS', "").split(',')
             self.INCLUDE_DOMAINS = False if include_domains == [''] else [include_domain.lower() for include_domain in include_domains]
-            in_domain_regex_patterns = os.getenv('INCLUDE_DOMAINS_REGEX', '').split('|')
-            in_domain_compiled_patterns = [re.compile(pattern) for pattern in in_domain_regex_patterns]
-            self.INCLUDE_DOMAINS_REGEX = False if in_domain_regex_patterns == [''] else in_domain_compiled_patterns
+            self.in_domain_regex_patterns = os.getenv('INCLUDE_DOMAINS_REGEX', '').split('|')
+            in_domain_compiled_patterns = [re.compile(pattern) for pattern in self.in_domain_regex_patterns]
+            self.INCLUDE_DOMAINS_REGEX = False if self.in_domain_regex_patterns == [''] else in_domain_compiled_patterns
             
             include_urls = os.getenv('INCLUDE_URLS', "").split(',')
             self.INCLUDE_URLS = False if include_urls == [''] else [include_urls.lower() for include_urls in include_urls]
-            in_url_regex_patterns = os.getenv('INCLUDE_URLS_REGEX', '').split('|')
-            in_url_compiled_patterns = [re.compile(pattern) for pattern in in_url_regex_patterns]
-            self.INCLUDE_URLS_REGEX = False if in_url_regex_patterns == [''] else in_url_compiled_patterns
+            self.in_url_regex_patterns = os.getenv('INCLUDE_URLS_REGEX', '').split('|')
+            in_url_compiled_patterns = [re.compile(pattern) for pattern in self.in_url_regex_patterns]
+            self.INCLUDE_URLS_REGEX = False if self.in_url_regex_patterns == [''] else in_url_compiled_patterns
             
             self.BASE_URLS = os.getenv('BASE_URLS', "").split(',')
             extract_link_type = os.getenv('EXTRACT_LINK_TYPE', "").split(',')
@@ -74,9 +74,9 @@ class Orchestrator:
         self.logging.info(f"EXCLUDE_LIST: {self.EXCLUDE_LIST}")
         self.logging.info(f"EXCLUDE: {self.EXCLUDE}")
         self.logging.info(f"INCLUDE_DOMAINS: {self.INCLUDE_DOMAINS}")
-        self.logging.info(f"INCLUDE_DOMAINS_REGEX: {in_domain_regex_patterns}")
+        self.logging.info(f"INCLUDE_DOMAINS_REGEX: {self.in_domain_regex_patterns}")
         self.logging.info(f"INCLUDE_URLS: {self.INCLUDE_URLS}")
-        self.logging.info(f"INCLUDE_URLS_REGEX: {in_url_regex_patterns}")
+        self.logging.info(f"INCLUDE_URLS_REGEX: {self.in_url_regex_patterns}")
         self.logging.info(f"BASE_URLS: {self.BASE_URLS}")
         self.logging.info(f"EXTRACT_LINK_TYPE: {self.EXTRACT_LINK_TYPE}")
         self.logging.info(f"CRAWL_URLS: {self.CRAWL_URLS}")
